cc3417f6cc0e8b9ba95a5feffeda28828b0c9edb
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-25 02:16:44 +0000

    Added routing to different pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return <Navbar bg="danger" variant="light" expand="md" fixed="top">
     <Container>
+    <NavLink to="/">
     <Navbar.Brand className={styles.NavBar}>Metalhub</Navbar.Brand>
+    </NavLink>
     <Nav className="ml-auto">
-      <Nav.Link className={styles.NavLink}><i className='fas fa-home'></i> Home</Nav.Link>
-      <Nav.Link className={styles.NavLink}><i className='fas fa-sign-in-alt'></i>  Sign Up</Nav.Link>
-      <Nav.Link className={styles.NavLink}><i className='fas fa-user-plus'></i>Sign In</Nav.Link>
+      <NavLink to="/" className={styles.NavLink}><i className='fas fa-home'></i> Home</NavLink>
+      <NavLink to="/signup" className={styles.NavLink}><i className='fas fa-sign-in-alt'></i>  Sign Up</NavLink>
+      <NavLink to="/signin" className={styles.NavLink}><i className='fas fa-user-plus'></i> Sign In</NavLink>
     </Nav>
     </Container>
   </Navbar>
