05dab8569573c8d79430e97ed2719944ff7c3203
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-26 03:43:51 +0000

    Adde conditional rendering to NavBar for logged in/out users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,65 @@ import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  );
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink to="/signup" className={styles.NavLink}><i className='fas fa-sign-in-alt'></i>  Sign Up</NavLink>
@@ -22,6 +75,7 @@ const NavBar = () => {
     <NavLink to="/">
     <Navbar.Brand className={styles.NavBar}>Metalhub</Navbar.Brand>
     </NavLink>
+    {currentUser && addPostIcon}
     <Nav className="ml-auto">
       <NavLink to="/" className={styles.NavLink}><i className='fas fa-home'></i> Home</NavLink>
       {currentUser ? loggedInIcons : loggedOutIcons}
