ea5ad6601d041b5db827ca357c1ee1e7a2d22bad
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-27 02:25:04 +0000

    Added infinite scrollbar functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -31,12 +31,12 @@ function PostPage() {
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Col className={`${appStyles.FontColour} py-2`} p-0 p-lg-2 lg={8}>
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={`${appStyles.Content} ${appStyles.FontColour}`}>Comments</Container>
       </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+      <Col lg={4} className={`${appStyles.FontColour} d-none d-lg-block p-0 p-lg-2`}>
         Popular profiles for desktop
       </Col>
     </Row>
