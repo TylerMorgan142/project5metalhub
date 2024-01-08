c9b78d2f191faf9b3e4aaa62756a509524fca468
TylerMorgan142 <tylermorgan142@gmail.com>
2024-01-08 08:13:45 +0000

    Added Album title to Review.js

--- src/pages/reviews/Review.js
+++ src/pages/reviews/Review.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/Review.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media } from "react-bootstrap";
@@ -16,13 +16,29 @@ const Review = (props) => {
     title,
     content,
     rating,
+    album: albumId,
     updated_at,
     reviewPage,
   } = props;
 
+  const [album, setAlbum] = useState(null);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchAlbum = async () => {
+      try {
+        const response = await axiosRes.get(`/albums/${albumId}/`);
+        console.log("Album data:", response.data);
+        setAlbum(response.data);
+      } catch (error) {
+        console.error("Error fetching album:", error);
+      }
+    };
+  
+    fetchAlbum();
+  }, [albumId]);
 
   const handleEdit = () => {
     history.push(`/reviews/${id}/edit`);
@@ -48,15 +64,15 @@ const Review = (props) => {
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
             {is_owner && reviewPage && (
-              <MoreDropdown
-              handleEdit={handleEdit}
-              handleDelete={handleDelete}
-            />
+              <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete} />
             )}
           </div>
         </Media>
       </Card.Body>
       <Card.Body>
+      {album && (
+          <p className={`${styles.font} text-center`}>Album: {album.title}</p>
+        )}
         {title && (
           <Link to={`/reviews/${id}`}>
             <Card.Title className={`${styles.font} text-center`}>{title}</Card.Title>
